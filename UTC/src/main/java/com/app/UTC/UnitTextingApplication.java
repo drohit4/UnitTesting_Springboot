3023a85b248d2e5516b8c04eb34d891d7073e628
drohit4 <deshmukhrohit773@gmail.com>
2025-01-12 21:59:50 +0530

    Initial Commit

---
+++ UTC/src/main/java/com/app/UTC/UnitTextingApplication.java
@@ -0,0 +1,13 @@
+package com.app.UTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTextingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTextingApplication.class, args);
+	}
+
+}
---
+++ UTC/src/test/java/com/app/UTC/UnitTextingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.UTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTextingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
